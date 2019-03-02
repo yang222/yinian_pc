2abae00c64741df58749ccf431bc55a8661d0968
boo <geeek001@outlook.com>
2019-03-02 16:15:36 +0800

    三月一日所提的bug修改

--- src/js/common.js
+++ src/js/common.js
@@ -1,13 +1,13 @@
 var config={
-    url:"http://www.yinian.com:8080/Admin/"
-    // url:"http://106.15.229.159:9001/Admin/"
+    // url:"http://www.yinian.com:8080/Admin/",
+    url:"http://106.15.229.159:9001/Admin/"
 }
 $(function(){
     $(".header .sq").click(function(){
         if($(".header .nav").css("display") == "none"){
             $(".header .nav").css("display","inline-block")
         }else{
-            if($(window).width() <= 800){
+            if($(window).width() <= 1150){
                 if($(".header .nav").css("height") =="0px" || $(".header .nav").css("height") ==0){
                     $(".header .nav").css("height","12rem")
                 }else{
@@ -20,15 +20,18 @@ $(function(){
             
         }
     })
-    $(".header li.cp").click(function(){
-        $(".header .nav").css("height","auto")
-        if($(this).hasClass("open")){
-            $(this).removeClass("open")
-            $(this).find(".child_nav").css("height",0)
-        }else{
-            $(this).addClass("open")
-        }
-    })
+    if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+        $(".header li.cp").click(function(){
+            $(".header .nav").css("height","auto")
+            if($(this).hasClass("open")){
+                $(this).removeClass("open")
+                $(this).find(".child_nav").css("height",0)
+            }else{
+                $(this).addClass("open")
+            }
+        })
+    }   
+    
 })
 
 //获取url参数
